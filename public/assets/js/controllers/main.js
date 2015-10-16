2f61a80035bfd8deeb697b933fba7c8fb42134d8
mkhairul <mkhairul@gmail.com>
2015-10-16 17:51:10 +0800

    search post

--- public/assets/js/controllers/main.js
+++ public/assets/js/controllers/main.js
@@ -94,7 +94,7 @@ app.controller('MainController',
   $scope.product = [];
   $scope.searchProduct = function(val)
   {
-      $http.get($rootScope.url + '/product/' + val).
+      $http.post($rootScope.url + '/product/', {'keyword': val}).
       success(function(data){
           if(data.status == 'error')
           {
