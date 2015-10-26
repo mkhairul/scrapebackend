c0eb9c93ce004bd3180e3ab31b1c8d9a47ec0cc8
mkhairul <mkhairul@gmail.com>
2015-10-27 01:46:20 +0800

    add assembly column to the product table

---
+++ database/migrations/2015_10_26_174533_add_assembly_product.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAssemblyProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+            $table->smallInteger('assembly')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+            $table->smallInteger('assembly');
+        });
+    }
+}
